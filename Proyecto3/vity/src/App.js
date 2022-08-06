93215b7d566456c4be6057d61fcf77cba2bf6226
codeswax <kevvsole@espol.edu.ec>
2022-08-06 17:59:58 -0500

    avance proyecto 3

---
+++ Proyecto3/vity/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Navbar, Brand, CTA} from './components'
+import {Footer, Blog, Possibilty, Features, WhatVITY, Header} from './containers'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className="db_bg">
+          <WhatVITY />
+        </div>
+    </div>
+  )
+}
+
+export default App
